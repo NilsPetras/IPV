9eebe4dc3e08cae697ad223d2a8370a2057addc2
Unknown <nils.petras@uni-konstanz.de>
2018-04-29 01:28:57 +0200

    vignette update

--- R/IPV.R
+++ R/IPV.R
@@ -7,15 +7,18 @@
 #'
 #' The input functions are used to format your raw data, so the model
 #' functions can use them.
+#' \code{\link{input_excel}}
 #'
 #' @section Model Functions:
 #'
 #' The model functions are used to calculate specific coordinates the plot
 #' functions draw from.
+#' \code{\link{model_items}} \code{\link{model_facets}} \code{\link{model_nested}}
 #'
 #' @section Plot Functions:
 #'
 #' The plot functions are used to create and customize the actual plots.
+#' \code{\link{plot_items}} \code{\link{plot_facets}} \code{\link{plot_nested}}
 #'
 #' @docType package
 #' @name IPV
