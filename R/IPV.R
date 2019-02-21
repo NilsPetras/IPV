4737312d6fc83eaadf09b7250131b482fda2ae5c
Nils Petras <nils.petras@mailbox.org>
2019-02-21 11:56:10 +0100

    docu updates, fade par
    updated the roxygen documentation on parameters (@details and @examples are still to be updated soon), also some minor clarity changes (file_name instead of filename, test_label instead of title, etc.), split the fade parameter for item charts into three parameters, one for each element

--- R/IPV.R
+++ R/IPV.R
@@ -1,28 +1,30 @@
 #' IPV: A package to create Item Pool Visualizations
 #'
-#' The IPV package provides two categories of important functions: input, and
-#' chart.
+#' The IPV package provides two sets of functions: input functions, and chart
+#' functions.
 #'
 #' @section Input Functions:
 #'
-#'   The input functions help preprocessing your data, so the other functions
-#'   can use them. \code{\link{input_manual_simple}}
-#'   \code{\link{input_manual_nested}} \code{\link{input_manual_process}}
-#'   \code{\link{input_excel}}
+#'   The input functions prepare your data for the chart functions. Input data manually using
+#'   loose input within R \code{\link{input_manual_simple}} (and possibly
+#'   \code{\link{input_manual_nested}}) combined with
+#'   \code{\link{input_manual_process}}. Or input data via MS Excel files and
+#'   \code{\link{input_excel}} as demonstrated in the examples.
 #'
 #' @section Chart Functions:
 #'
-#'   Chart functions create a ggplot2 object (the chart) and optionally save
-#'   this object to a file. There are three types of charts.
-#'   \code{\link{item_chart}} \code{\link{facet_chart}}
+#'   Chart functions create a ggplot2 object (the chart) and save it as a file.
+#'   There are three types of charts. All functions have several parameters for
+#'   customization. Yet, there are sensible default values for everything but the
+#'   data. \code{\link{item_chart}} \code{\link{facet_chart}}
 #'   \code{\link{nested_chart}}
 #'
 #' @section Workflow:
 #'
-#'   Prepare your data using input functions or by hand. Choose a chart type and
-#'   use the proper chart function. Then \enumerate{\item choose parameter
-#'   values of the chart function \item check chart appearance \item repeat
-#'   until satisfied}
+#'   Prepare your data using the input functions. Choose the chart type. Use the
+#'   chart function with your data and defaults. Then \enumerate{\item change
+#'   the parameter values of the chart function \item check the chart appearance by
+#'   opening the created file \item repeat until you are satisfied with the result}
 #'
 #' @docType package
 #' @name IPV
